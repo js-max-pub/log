5b39c9a12e4db6cc80204ad93403159df4ab5b09
Max Fechner <max.fechner@gmail.com>
2022-04-26 10:52:27 +0200

    .

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
-export { pretty, percent } from '../number/mod.js'
+// export { pretty, percent } from '../number/mod.js'
 
 export class Log {
 	static ERROR = 1
@@ -7,6 +7,7 @@ export class Log {
 	static DEBUG = 4
 
 	static level = Log.DEBUG;
+	static callback = null
 
 	constructor(p = '') {
 		// try { this.prefix = new URL(p).toString().split('/').slice(-1)[0].slice(0,-3) }
@@ -15,11 +16,16 @@ export class Log {
 	}
 
 	text(type, ...p) {
+		// console.log('level',Log.level,Log[type])
 		if (Log.level < Log[type]) return
+		// console.log("DUR",p)
 		let dur = Date.now() - p.slice(-1)[0]
 		if (dur < 1_000_000) p = p.slice(0, -1)
+		// console.log("DUR",dur)
 		// console.log(getStackTrace().filter(x=>!x.startsWith('file:')))
-		standardLog(this.prefix, getStackTrace().filter(x => !x.startsWith('file:'))[2], dur, ...p)
+		let func = getStackTrace().filter(x => !x.startsWith('file:'))[0]?.replace('Module.', '')?.replace('Object.','')?.replace('[as function]','')?.trim()
+		let v = standardLog(this.prefix, func, dur, ...p)
+		Log.callback?.(v.filter(x => !x?.startsWith?.('#')).join(' '))
 	}
 
 	debug(...p) { this.text('DEBUG', '#999', ...p) }
@@ -29,10 +35,11 @@ export class Log {
 }
 
 
+
 const wrap = (a, x, b) => x ? a + x + b : ''
 export function standardLog(prefix, caller, duration, ...p) {
 	let time = new Date().toISOString().slice(11, 19)
-	colorLog(
+	return colorLog(
 		'#999', time,
 		'#bbb', wrap('[', prefix, ']'),
 		'#999', wrap('{', caller, '}'),
@@ -46,15 +53,17 @@ export function colorLog(...p) {
 	let text = p.map(x => x?.startsWith?.('#') ? '%c' : x).join(' ').replaceAll('%c ', '%c')
 	let color = p.filter(x => x?.startsWith?.('#')).map(x => `color:${x}`)
 	console.log(text, ...color)
+	return p
 }
 
 
 export function getStackTrace() {
 	// try { new URL() } catch (e) { var st = e.stack }
 	try { a.b = 1 } catch (e) { var st = e.stack }
+	// console.log(st.split('\n').map(x => x.slice(7).split('(')[0].trim()))
 	st = st.split('\n')
 		.map(x => x.slice(7).split('(')[0].trim())
-		.slice(5)
+		.slice(4) // or 5?
 	return st
 }
 
